67f2d1c0790831711523ed490282b23d70b8db99
vivekky57 <vivekky57@gmail.com>
2021-08-23 23:59:14 +0530

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,29 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
+
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"),75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"),60)
 let revealOnScroll = new RevealOnScroll();
 
 let mobileMenu = new MobileMenu();
+let modal
+
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if(typeof modal == undefined){
+      import(/*webpackChunkName: "modal" */'./modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(),20) // waiting for 20 millsecond so to create a modal and inject to our javascript file
+      }).catch( () => console.log("There was a problem."))
+
+    } else{
+      modal.openTheModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
