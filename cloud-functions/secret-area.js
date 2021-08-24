823f316803738e1384628058445bb2814ffdd80f
vivekky57 <vivekky57@gmail.com>
2021-08-24 11:07:08 +0530

    Updated cloud function check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,31 @@
 exports.handler = function(event,context, callback){
-    callback(null, {
-        statusCode:200,
-        body:"welcome to the super secret area"
-    })
-}
+    const secretContent =`
+    <h3>Welcome To The Sceret area.</h3>
+    <p>Here we can tell  you that sky is<strong>blue</strong>, and two plus two equal to four</p>
+    `
     
+    let body
+
+    if( event.body){
+        body = JSON.parse(event.body)
+
+    } 
+    else {
+        body = {}
+
+    }
+
+    if(body.password = "javascript"){
+        callback(null, {
+            statusCode:200, //sucesss code=200, not found=404
+            body: secretContent
+        })
+    } 
+    else{
+        callback(null, {
+            statusCode:401, //unauthorized
+        })
+    }
+
+   
+}
